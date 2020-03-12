1ca34d12f5c396e0097ac4d5bddb092d99ffbbbd
CallumBrooks453 <55701050+CallumBrooks453@users.noreply.github.com>
2020-03-12 00:15:23 +0000

    Added Sideways Movement and Collision with path

--- js/Main_Scene.js
+++ js/Main_Scene.js
@@ -1,30 +1,54 @@
-let canvas = document.querySelector('canvas');
-let context = canvas.getContext('2d');
+class BaseScene extends Phaser.Scene {
+    constructor(config) {
+        super(config);
+        this.scrollSpeed = 200
+    }
+    preload() {
+        this.load.image('road', 'assets/png/level-1.png')
+        this.load.image("player", "assets/png/car_player.png")
+    }
+    create() {
+        //Load Road
+        this.road = this.physics.add.sprite(160, -205, "road")
+        this.road2 = this.physics.add.sprite(160, 200, "road")
+        //Load Player
+        this.player = this.physics.add.sprite(162, 350, "player")
+        this.player.setCollideWorldBounds(true)
+        //Movement
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
 
-let road = new Image;
-road.src = './assets/png/level-1.png';
+    update() {
+        //Road Movement
+        this.road.setVelocityY(this.scrollSpeed)
+        this.road2.setVelocityY(this.scrollSpeed)
+        if (this.road.body.y > 405) {
+            this.road.body.y = -405
+        }
+        if (this.road2.body.y > 405) {
+            this.road2.body.y = -405
+        }
+        //Player Controls and Collision with Path
+        if (this.player.x >= 207){} else if (this.input.activePointer.x >= 205 & this.input.activePointer.isDown) {
+            this.player.body.x += 45
+            if (this.input.activePointer.isDown = false) {
+                this.player.body.x = 0
+            }
+        }
 
-//Road Animation
-road.onload = () => {
-    requestAnimationFrame(gameLoop);
+        if (this.player.x <= 160){} else if (this.input.activePointer.x <= 205 & this.input.activePointer.isDown) {
+            this.player.body.x -= 45
+            if (this.input.activePointer.isDown = false) {
+                this.player.body.x = 0
+            }
+            // //Speed Controler
+            // if(this.scrollSpeed <= 1){
+            //     this.scrollSpeed = 0
+            // }
+
+            // if(this.scrollSpeed >= 400){SW
+            //     this.scrollSpeed = 400
+            // }
+        }
+    }
 }
-
-let yOffset = -410;
-
-function gameLoop() {
-
-    if (yOffset >= 0) yOffset = -410;
-
-    context.drawImage(road, 0, yOffset);
-    context.drawImage(road, 0, yOffset + 410);
-    context.drawImage(road, 0, yOffset + 820);
-    context.drawImage(player, 130, 205)
-
-    yOffset += 10;
-
-    requestAnimationFrame(gameLoop);
-}
-
-//Load Player
-let player = new Image;
-player.src = './assets/png/car_player.png'
