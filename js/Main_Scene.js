2f1a0fb81636f55f90dbb82a3ebfb3b1d137bf0f
CallumBrooks453 <55701050+CallumBrooks453@users.noreply.github.com>
2020-03-12 12:20:38 +0000

    Improved Scale and Added non working Fuel Bar

--- js/Main_Scene.js
+++ js/Main_Scene.js
@@ -4,18 +4,29 @@ class BaseScene extends Phaser.Scene {
         this.scrollSpeed = 200
     }
     preload() {
-        this.load.image('road', 'assets/png/level-1.png')
-        this.load.image("player", "assets/png/car_player.png")
+        this.load.image('road', 'assets/png/level-1.png');
+        this.load.image("player", "assets/png/car_player.png");
+
+        //UI Load
+        this.load.image("fuelBar", 'assets/png/fuel-bar-inner.png');
+        this.load.image('fuelOutline', 'assets/png/fuel-bar-outer.png', )
+        
     }
     create() {
         //Load Road
-        this.road = this.physics.add.sprite(160, -205, "road")
-        this.road2 = this.physics.add.sprite(160, 200, "road")
+        this.road = this.physics.add.sprite(160, -205, "road");
+        this.road2 = this.physics.add.sprite(160, 200, "road");
         //Load Player
-        this.player = this.physics.add.sprite(162, 350, "player")
-        this.player.setCollideWorldBounds(true)
-        //Movement
-        this.cursors = this.input.keyboard.createCursorKeys();
+        this.player = this.physics.add.sprite(162, 350, "player");
+        this.player.setCollideWorldBounds(true)  
+        //Load Fuel
+        this.add.text(5,3,'Fuel:', {
+            font: '15px Arial'   
+        });
+        this.fuelBar = this.physics.add.image(48, 24, 'fuelBar')
+        this.fuelBar.setScale(0.22)
+        this.fuelBarOutline = this.physics.add.image(48, 24, 'fuelOutline')
+        this.fuelBarOutline.setScale(0.22)
     }
 
     update() {
@@ -29,14 +40,14 @@ class BaseScene extends Phaser.Scene {
             this.road2.body.y = -405
         }
         //Player Controls and Collision with Path
-        if (this.player.x >= 207){} else if (this.input.activePointer.x >= 205 & this.input.activePointer.isDown) {
+        if (this.player.x >= 207) {} else if (this.input.activePointer.x >= 160 & this.input.activePointer.isDown) {
             this.player.body.x += 45
             if (this.input.activePointer.isDown = false) {
                 this.player.body.x = 0
             }
         }
 
-        if (this.player.x <= 160){} else if (this.input.activePointer.x <= 205 & this.input.activePointer.isDown) {
+        if (this.player.x <= 160) {} else if (this.input.activePointer.x <= 159 & this.input.activePointer.isDown) {
             this.player.body.x -= 45
             if (this.input.activePointer.isDown = false) {
                 this.player.body.x = 0
@@ -51,4 +62,6 @@ class BaseScene extends Phaser.Scene {
             // }
         }
     }
+
+
 }
